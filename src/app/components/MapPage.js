9c3cd52b2b41d5035dc0cb7e6ded297f281728ef
Your Name <you@example.com>
2024-03-26 15:30:29 +0800

    add button that redirect to view activity page

--- src/app/components/MapPage.js
+++ src/app/components/MapPage.js
@@ -5,8 +5,10 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import MarkerIcon from '../../../node_modules/leaflet/dist/images/marker-icon.png';
 import MarkerShadow from '../../../node_modules/leaflet/dist/images/marker-shadow.png';
+import { useRouter } from 'next/navigation';
 
 const MapPage = () => {
+    const router = useRouter();
     const [markers, setMarkers] = useState([]);
     
     useEffect(() => {
@@ -30,7 +32,7 @@ const MapPage = () => {
     };
 
     return (
-        <div style={{ height: '80vh', width: '80vw' }}>
+        <div style={{ height: '80vh', width: '82vw' }}>
             <MapContainer style={{ height: '100%', width: '100%' }} center={[2.921300, 101.655900]} zoom={16} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -54,6 +56,7 @@ const MapPage = () => {
                             <p>{marker.description}</p>
                             <p>Activity ID: {marker.activityId}</p>
                             <p>Latitude: {marker.position[0]}, Longitude: {marker.position[1]}</p>
+                            <button style={{ color:'white' }} class="btn btn-success" onClick={() => router.push(`/activity/view/${marker.key}`)}>Details</button>
                         </Popup>
                     </Marker>
                 ))}
